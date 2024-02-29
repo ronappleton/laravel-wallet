69683b52a0e472b2f55991555540296bc62ac6d2
Ron Appleton <ron.appleton@gmail.com>
2024-02-29 09:56:57 +0000

    Re-order Bindings

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -8,8 +8,10 @@ use Appleton\LaravelWallet\Contracts\CurrencyService as CurrencyContract;
 use Appleton\LaravelWallet\Contracts\WalletMeta as WalletMetaContract;
 use Appleton\LaravelWallet\Contracts\WalletModel as WalletModelContract;
 use Appleton\LaravelWallet\Contracts\WalletService as WalletContract;
+use Appleton\LaravelWallet\Contracts\WalletTransactionModel as WalletTransactionModelContract;
 use Appleton\LaravelWallet\Helpers\WalletMeta;
 use Appleton\LaravelWallet\Models\Wallet as WalletModel;
+use Appleton\LaravelWallet\Models\WalletTransaction;
 use Appleton\LaravelWallet\Services\Currency;
 use Appleton\LaravelWallet\Services\Wallet;
 
@@ -17,28 +19,27 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     public function register(): void
     {
-        $this->app->bind(
-            WalletContract::class,
-            Wallet::class
-        );
-
-        $this->app->bind(
-            CurrencyContract::class,
-            Currency::class,
-        );
-
-        $this->app->bind(
-            WalletMetaContract::class,
-            WalletMeta::class,
-        );
-
         /** @var string $walletModel */
         $walletModel = config('wallet.models.wallet.model', WalletModel::class);
 
-        $this->app->bind(
-            WalletModelContract::class,
-            $walletModel,
-        );
+        // Wallet Model Binding
+        $this->app->bind(WalletModelContract::class, $walletModel,);
+
+        /** @var string $walletTransactionModel */
+        $walletTransactionModel =
+            config('wallet.models.wallet_transaction.model', WalletTransaction::class);
+
+        // Wallet Transaction Model Binding
+        $this->app->bind(WalletTransactionModelContract::class, $walletTransactionModel);
+
+        // Wallet Service Binding
+        $this->app->bind(WalletContract::class, Wallet::class);
+
+        // Currency Service Binding
+        $this->app->bind(CurrencyContract::class, Currency::class,);
+
+        // WalletMeta Binding
+        $this->app->bind(WalletMetaContract::class, WalletMeta::class,);
     }
 
     public function boot(): void
